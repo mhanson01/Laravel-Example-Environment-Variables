e0e407e901c037e97405b5f6c03249f12c7fc85d
Mark Hanson <mhanson01@gmail.com>
2014-03-23 18:46:39 -0400

    Example App Environments

---
+++ .env.sample.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+*  On your development server:
+*    Copy this file to .env.local.php and save local development settings
+*
+*  On your production server:
+*    You must also create this file on the server!
+*    It will not be saved with your git repo!
+*    Copy this file to .env.php and save production settings
+*/
+
+return [
+
+	'MYSQL_HOST'      => '',
+	'MYSQL_DATABASE'  => '',
+	'MYSQL_USERNAME'  => '',
+	'MYSQL_PASSWORD'  => '',
+
+];
